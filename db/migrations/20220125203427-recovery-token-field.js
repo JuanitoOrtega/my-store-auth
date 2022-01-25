bc62530419e69f7a9fbfb696a02de06dbecf0ea5
Juanito Ortega <ortegaj83@gmail.com>
2022-01-25 19:45:29 -0400

    Deploy heroku

--- db/migrations/20220125203427-recovery-token-field.js
+++ db/migrations/20220125203427-recovery-token-field.js
@@ -4,11 +4,11 @@ const { USER_TABLE } = require('../models/user.model');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn(USER_TABLE, 'recovery_token', {
-      field: 'recovery_token',
-      allowNull: true,
-      type: Sequelize.DataTypes.STRING
-    });
+    // await queryInterface.addColumn(USER_TABLE, 'recovery_token', {
+    //   field: 'recovery_token',
+    //   allowNull: true,
+    //   type: Sequelize.DataTypes.STRING
+    // });
   },
 
   down: async (queryInterface) => {
