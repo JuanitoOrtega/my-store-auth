91a3d1842638ccd1cdbea03dd7f1fb440a9d7142
Juanito Ortega <ortegaj83@gmail.com>
2022-01-25 18:04:32 -0400

    Validando tokens para cambio de contraseña

--- services/auth.service.js
+++ services/auth.service.js
@@ -52,6 +52,21 @@ class AuthService {
         return rta;
     }
 
+    async changePassword(token, newPassword) {
+        try {
+            const payload = jwt.verify(token, config.jwtSecret);
+            const user = await service.findOne(payload.sub);
+            if (user.recoveryToken !== token) {
+                throw boom.unauthorized('Invalid token');
+            }
+            const hash = await bcrypt.hash(newPassword, 10);
+            await service.update(user.id, { password: hash, recoveryToken: null });
+            return { message: 'Password changed' };
+        } catch (error) {
+            throw boom.unauthorized('Invalid or expired token');
+        }
+    }
+
     async sendMail(infoMail) {
         const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
